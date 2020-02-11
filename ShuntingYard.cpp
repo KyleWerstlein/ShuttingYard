858a15df93199f8d6893ceb1322fe6a71f868acf
U-BSD\321341 <321341@SHS-10DECC84C.beaverton.k12.or.us>
2020-02-11 10:48:40 -0800

    very frustrating

--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -7,28 +7,73 @@
 
 using namespace std;
 
+void stack_peek(Node* head);
+Node* stack_pop(Node* head);
 void stack_push(Node* node, Node* head);
+void test(int testNum);
 
 int main() {
   char input[256];
-  Node* head = new Node();
   Node* stackHead = new Node();
-  Node* currentNode;
-  Node* previousNode = head;
+  stackHead = 0;
+  Node* stackCurrentNode;
+  int nodeCount = 0;
+  Node* stackPreviousNode = stackHead;
   cin.getline(input, 256);
   for(int i = 0; i < strlen(input); i++) {
     if(input[i] != ' ') {
-      currentNode = new Node();
-      previousNode->setRight(currentNode);
-      currentNode->setLeft(previousNode);
-      currentNode->setData(input[i]);
-      if(currentNode->getData() == '*' || currentNode->getData() == '-' || currentNode->getData() == '+' || currentNode->getData() == '/') {
-	stack_push(currentNode, stackHead);
+	  if(input[i] == '*' || input[i] == '-' || input[i] == '+' || input[i] == '/') {
+		  stackCurrentNode = new Node();
+		  stackCurrentNode->setData(input[1]);
+		  if(stackHead == 0) {
+			  stackHead = stackCurrentNode;
+			  stackPreviousNode = stackHead;
+			  nodeCount++;
+		  }
+		  else if(nodeCount == 1) {
+		    stackHead->setRight(stackCurrentNode);
+		  }
+		  else {
+			stackPreviousNode->setRight(stackCurrentNode);
+			stackCurrentNode->setLeft(stackPreviousNode);
+			stack_push(stackCurrentNode, stackHead);
+			stackPreviousNode = stackCurrentNode;
+		  }
+      
       }
-      previousNode = currentNode;
+	  else if (input[i] > 47 && input[i] < 58) {
+		  
+	  }
     }
   }
+  stack_peek(stackHead);
   return 0;
+}
+
+void stack_peek(Node* head) {
+  Node* peek;
+  peek = stack_pop(head);
+  //char printData = peek->getData();
+  cout << peek->getData() << endl;
+  stack_push(peek, head);
+}
+
+Node* stack_pop(Node* head) {
+  bool loop = true;
+  Node* leftP;
+  Node* copy;
+  while(loop) {
+    if(head->getRight() != 0) {
+      head = head->getRight();
+    }
+    else {
+      copy = head;
+      leftP = head->getLeft();
+      head = 0;
+      leftP->setRight(0);
+      return copy;
+    }
+  }
 }
 
 void stack_push(Node* node, Node* head) {
@@ -42,4 +87,8 @@ void stack_push(Node* node, Node* head) {
       loop = false;
     }
   }
+}
+
+void test(int testNum) {
+  cout << "test " << testNum << endl;
 }
